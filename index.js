15d13232872786e42005379412b268cc6ecfd796
elega9t <dubey.yogesh@gmail.com>
2021-04-15 17:56:55 +0100

    update to use promise and multiple PUTs

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 (async function() {
   const fs = require('fs')
+  const split = require('split')
+  const stream = require('stream')
   const si = require('search-index')
   const memdown = require('memdown')
 
   const { PUT, QUERY } = await si({
     db: memdown('journals')
-  })
+  });
 
-  const batch = fs.readdirSync('journals').map(
-    j => JSON.parse(fs.readFileSync('journals/' + j))
-  )
-  
-  await PUT(batch)
+  function readFile(file) {
+    return new Promise((resolve, reject) => {
+      let readStream = fs.createReadStream('journals/' + file).pipe(split(JSON.parse, null, { trailing: false }));
 
-  console.log(JSON.stringify(await QUERY({
-    OR: ['dag', 'emmey', 'dorie', 'cordell', 'gregoire']
-  }), null, 2))
+      const promises = [];
+      readStream.on('data', (entry) => {
+        promises.push(PUT([ entry ]));
+      });
+
+      readStream.on('end', () => {
+        Promise.all(promises)
+          .then(() => resolve());
+      });
+
+      readStream.on('error', (err) => {
+        reject(err)
+      });
+    });
+  }
+
+  const promises = fs.readdirSync('journals').map(file => readFile(file));
+
+  await Promise.all(promises);
+
+  const results = await QUERY(
+    { SEARCH: [ 'Chaudron' ] },
+    { DOCUMENTS: true, PAGE: { NUMBER: 0, SIZE: 20 } }
+  );
+
+  console.log(JSON.stringify(results));
   
 }())
